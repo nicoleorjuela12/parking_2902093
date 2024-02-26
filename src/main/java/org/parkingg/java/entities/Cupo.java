b3df3b424393cd39ba3a7e08c5d06b8cb30e627a
AndresO <ochoaperezandreso1@gmail.com>
2024-02-26 14:23:01 -0500

    entidades listas para el main

--- src/main/java/org/parkingg/java/entities/Cupo.java
+++ src/main/java/org/parkingg/java/entities/Cupo.java
@@ -2,4 +2,15 @@ package org.parkingg.java.entities;
 
 public class Cupo {
 
+    public char nombre;
+
+    public Cupo() {
+    }
+
+    public Cupo(char nombre) {
+        this.nombre = nombre;
+    }
+
+    
+
 }
--- src/main/java/org/parkingg/java/entities/Pago.java
+++ src/main/java/org/parkingg/java/entities/Pago.java
@@ -1,5 +1,40 @@
 package org.parkingg.java.entities;
+import java.time.LocalDateTime;
 
 public class Pago {
+
+    // Atributos intrinsecos
+    public LocalDateTime fechaHoraInicio;
+    public LocalDateTime fechaHoraFin;
+    public Double valor;
+
+    //Atributos de relacion:
+
+    public Vehiculo vehiculo;
+    public Cupo cupo;
+
+
+    //constructor por defecto
+    
+    public Pago() {
+    }
+
+    //constructor parametrizado
+
+
+    public Pago(LocalDateTime fechaHoraInicio, LocalDateTime fechaHoraFin, Double valor, Vehiculo vehiculo, Cupo cupo) {
+        this.fechaHoraInicio = fechaHoraInicio;
+        this.fechaHoraFin = fechaHoraFin;
+        this.valor = valor;
+        this.vehiculo = vehiculo;
+        this.cupo = cupo;
+    }
+
+
+
+    
+    
+
+
 
 }
